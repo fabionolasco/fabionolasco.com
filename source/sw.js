c01b81f868e39422dee5e0de64a208beea57ce4a
Fabio Nolasco <gomidefabio@gmail.com>
2018-05-17 09:06:35 -0400

    Added npm preprocessors and includes

---
+++ source/sw.js
@@ -0,0 +1,25 @@
+var cacheName = 'fabio-nolasco-website';
+var filesToCache = [
+    '/',
+    '/index.html',
+    '/about.html',
+    '/contact.html',
+    '/book.html'
+];
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open(cacheName).then(function (cache) {
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+self.addEventListener('activate', event => {
+    event.waitUntil(self.clients.claim());
+});
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request, { ignoreSearch: true }).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
